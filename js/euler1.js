4b9577490272ef9de53c882da3df8be63ed0480e
Stephen Berkner <sberkner18@gmail.com>
2022-02-11 19:09:58 -0500

    Euler1 Syntax

--- js/euler1.js
+++ js/euler1.js
@@ -1,7 +1,7 @@
-total = 0
+total = 0;
 for (let i = 0; i < 1000; i++) {
     if (((i % 3) == 0) || ((i % 5) == 0)) {
-        total += i
+        total += i;
     } 
 }
-console.log(total)
+console.log(total);
